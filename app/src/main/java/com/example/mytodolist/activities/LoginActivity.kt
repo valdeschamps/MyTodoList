15cf2b27496d7521a628ff38c880c5ecfb8589b6
sirious2 <valentin.deschamps38@gmail.com>
2019-02-16 01:31:25 +0100

    sign in and register fragments created

--- app/src/main/java/com/example/mytodolist/activities/LoginActivity.kt
+++ app/src/main/java/com/example/mytodolist/activities/LoginActivity.kt
@@ -1,12 +1,13 @@
-package com.example.mytodolist
+package com.example.mytodolist.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mytodolist.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
---
+++ app/src/main/java/com/example/mytodolist/activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.mytodolist.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mytodolist.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
